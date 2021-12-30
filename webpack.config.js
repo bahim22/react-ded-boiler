8bcb53f2cc0c323a81c6c5628c32df79f30b6c27
dionysus <dionysus@isitymarket.onmicrosoft.com>
2021-12-30 14:02:13 -0500

    babel/webpack

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { WebpackPluginServe } = require('webpack-plugin-serve');
+
+const port = process.env.PORT || 3000;
+
+module.exports = {
+  // Webpack configuration goes here
+};
